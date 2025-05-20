07a3656432f7dab94b2fd7555e3f719ca739602f
SunaUezuri <wesley.jane.santos@gmail.com>
2025-05-20 18:59:24 -0300

    Criando o projeto base

---
+++ src/main/java/br/com/fiap/geomottu/GeomottuApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.geomottu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeomottuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeomottuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/geomottu/GeomottuApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.geomottu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeomottuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
